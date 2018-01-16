a98b26334da43f0521133fa3fed26bf5e50b8d0c
Martin Chapman <martin.chapman@kcl.ac.uk>
2018-01-16 15:12:40 +0000

    Recursive JSON printing.

--- fhir-ehr-adapter/src/main/python/SMARTOnFHIRContainer/Main.py
+++ fhir-ehr-adapter/src/main/python/SMARTOnFHIRContainer/Main.py
@@ -1,15 +1,17 @@
 from BaseHTTPServer import HTTPServer
-import uuid, requests
+import uuid, requests, json
 
 from EHR.SystmOne import SystmOne
 from FHIR.FHIRServer import FHIRServer
 from FHIR.FHIRConstants import FHIRConstants
+from Translation.FHIRTranslation import FHIRTranslation
 
 if __name__ == "__main__":
     
-    #sysone = SystmOne()
-    #sysone.getPatientRecord("4917111072");
+    #SystmOne().getPatientRecord("4917111072");
     
-    server_address = ('', 8080)
-    httpd = HTTPServer(server_address, FHIRServer)
-    httpd.serve_forever()
+    #server_address = ('', 8080)
+    #httpd = HTTPServer(server_address, FHIRServer)
+    #httpd.serve_forever()
+    
+    FHIRTranslation.translatePatient()
