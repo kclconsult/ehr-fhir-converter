3a4d66eec6e268401e647fddb5af250ef30c4317
Martin Chapman <martin.chapman@kcl.ac.uk>
2018-03-06 14:54:11 +0000

    Refined similarity metric B.

--- fhir-ehr-adapter/src/main/python/SMARTOnFHIRContainer/Translation/FHIRTranslation.py
+++ fhir-ehr-adapter/src/main/python/SMARTOnFHIRContainer/Translation/FHIRTranslation.py
@@ -1,4 +1,4 @@
-import json, inspect, collections, xml.etree.ElementTree
+import json, inspect, collections, xml.etree.ElementTree, sys
 from pprint import pprint
 from difflib import SequenceMatcher
 from nltk.corpus import wordnet
@@ -12,10 +12,9 @@ class FHIRTranslation():
     @staticmethod
     def getXMLElements(root, set):
         
-        set.add(root.tag)
-        
         for elem in root.getchildren():
-            FHIRTranslation.getXMLElements(elem, set)
+            set.add(elem.tag);
+            FHIRTranslation.getXMLElements(elem, set);
             
         return set
        
@@ -28,28 +27,21 @@ class FHIRTranslation():
     
     # Similarity Metric B
     @staticmethod
-    def semanticSimilarity(ehrAttribute, fhirAttribute, textSimilarityThreshold):
-       
-        similarityMatches = 0
-        generatedSynonyms = 0;
+    def semanticSimilarity(ehrAttribute, fhirAttribute):
+        
+        # https://docs.python.org/2/library/sys.html#sys.maxint
+        highestSimilarity = -sys.maxint - 1;
        
         # wordnet requires word separation by underscore, whereas EHR XML responses (for TPP at least) use camelCase.
         for set in wordnet.synsets(Utilities.captialToSeparation(ehrAttribute)):
            
             for word in set.lemma_names():
-               
-               # Get similarity between synonym for ehrAttribute and fhirAttribute. If this is over a given threshold, mark as a semantic match.
-                if FHIRTranslation.textSimilarity(word, fhirAttribute) > textSimilarityThreshold:
-                    similarityMatches = similarityMatches + 1
-                   
-                generatedSynonyms = generatedSynonyms + 1;
-               
-        # Take synonyms that matched over total possible synonyms as this metric:
-        if generatedSynonyms > 0:
-            return similarityMatches / generatedSynonyms
+                
+                # Get similarity between synonym for ehrAttribute and fhirAttribute. If this is over a given threshold, mark as a semantic match.
+                if FHIRTranslation.textSimilarity(word, fhirAttribute) > highestSimilarity:
+                    highestSimilarity = FHIRTranslation.textSimilarity(word, fhirAttribute);
         
-        else:
-            return 0
+        return highestSimilarity;
     
     # Similarity Metric C
     # Split ehr and fhir attributes into individual words, and see how many match. Would tackle cases such as DateOfBirth and BirthDate.
@@ -60,19 +52,23 @@ class FHIRTranslation():
     def translatePatient():
         
         # Get JSON representation of FHIR resource (Patient).
-        patientJSON = Utilities.JSONfromFHIRClass(Patient, False);
+        #patientJSON = Utilities.JSONfromFHIRClass(Patient, False);
         
         # Get patient record from EHR
         # SystmOne().getPatientRecord("4917111072");
         
-        #Find candidate in FHIR representation for each EHR XML attribute.
-        for ehrAttribute in FHIRTranslation.getXMLElements(xml.etree.ElementTree.parse('../../../../resources/ehr-response-extract.xml').getroot(), set()):
+        # Find candidate in FHIR representation for each EHR XML attribute.
+        for ehrAttribute in FHIRTranslation.getXMLElements(xml.etree.ElementTree.parse('../../../../resources/ehr-response-extract.xml').find("Response"), set()):
             
             for fhirAttribute in Utilities.getReplaceJSONKeys(json.load(open('../../../../resources/patient-fhir-extract.json')), ""):
-            
-                if ( FHIRTranslation.semanticSimilarity(ehrAttribute, fhirAttribute, 0.5) > 0.5 ) :
-                    
-                    print ehrAttribute + " " + fhirAttribute
+                
+                if ( FHIRTranslation.semanticSimilarity(ehrAttribute, fhirAttribute) > 0.8 ):
+                    print ehrAttribute + " " + fhirAttribute;
+                    break;
+                
+                if ( FHIRTranslation.textSimilarity(ehrAttribute, fhirAttribute) > 0.8 ):
+                    print ehrAttribute + " " + fhirAttribute;
+                    break; 
                 
             
         # Match components of patient record from EHR to components from JSON representation
