241b9d57049bd8f52e22df4695bb2af942d8e40c
Martin Chapman <contact@martinchapman.co.uk>
2019-03-12 16:38:52 +0000

    Starlette basic functionality fix.

--- src/main.py
+++ src/main.py
@@ -6,19 +6,19 @@ from translation.translationConstants import TranslationConstants
 from translation.similarityMetrics import SimilarityMetrics
 from utils.utilities import Utilities
 
-#from starlette.applications import Starlette
-#import uvicorn
+from starlette.applications import Starlette
+from starlette.responses import JSONResponse
+import uvicorn
 
-#app = Starlette(debug=True)
+app = Starlette(debug=True)
 
-#@app.route("/translate")
-def translate():
+@app.route("/translate")
+def translate(request):
     FHIRTranslation.translatePatient();
 
-#@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+async def homepage(request):
+    return JSONResponse({'hello': 'world'})
 
 if __name__ == '__main__':
-    #uvicorn.run(app, host='0.0.0.0', port=8000)
-    translate();
+    uvicorn.run(app, host='0.0.0.0', port=8000)
